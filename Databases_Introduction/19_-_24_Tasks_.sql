a1927cae15b90b757e56f2be44a7f0e9c601be63
Stan <dimitrov.s.dev@gmail.com>
2023-01-17 00:48:44 +0200

    Databases_Introduction

--- Databases_Introduction/19_-_24_Tasks_.sql
+++ Databases_Introduction/19_-_24_Tasks_.sql
@@ -9,3 +9,18 @@ SELECT * FROM Towns
 SELECT * FROM Departments
 
 SELECT * FROM Employees
+
+-- Task 20 => Basic Select All Fields and Order Them
+--Modify the queries from the previous problem by sorting:
+--· Towns - alphabetically by name
+--· Departments - alphabetically by name
+--· Employees - descending by salary
+
+SELECT * FROM Towns
+ORDER BY name
+
+SELECT * FROM Departments
+ORDER BY name
+
+SELECT * FROM Employees
+ORDER BY salary DESC
