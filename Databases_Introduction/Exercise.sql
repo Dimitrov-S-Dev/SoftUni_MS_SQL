a3e6bd17195e0da5d5c799d2574584788c1b94f8
Stan <dimitrov.s.dev@gmail.com>
2023-01-16 20:22:09 +0200

    Databases_Introduction

--- Databases_Introduction/Exercise.sql
+++ Databases_Introduction/Exercise.sql
@@ -1,2 +1,17 @@
 -- Task 1
 CREATE DATABASE [Minions]
+
+-- Task 2
+
+CREATE Table [Minions] (
+	[Id] INT PRIMARY KEY,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Age] INT NOT NULL
+
+)
+
+CREATE Table [Towns] (
+	[Id] INT PRIMARY KEY,
+	[Name] NVARCHAR(50) NOT NULL
+
+)
