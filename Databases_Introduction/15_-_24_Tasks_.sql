ff63886e4fd8aa14225527a518082780594f8e79
Stan <dimitrov.s.dev@gmail.com>
2023-01-17 00:29:27 +0200

    Databases_Introduction

--- Databases_Introduction/15_-_24_Tasks_.sql
+++ Databases_Introduction/15_-_24_Tasks_.sql
@@ -1,3 +1,129 @@
+-- Task 15
+-- create Hotel database with the following entities:
+--· Employees (Id, FirstName, LastName, Title, Notes)
+--· Customers (AccountNumber, FirstName, LastName, PhoneNumber,
+--EmergencyName, EmergencyNumber, Notes)
+--· RoomStatus (RoomStatus, Notes)
+--· RoomTypes (RoomType, Notes)
+--· BedTypes (BedType, Notes)
+--· Rooms (RoomNumber, RoomType, BedType, Rate, RoomStatus, Notes)
+--· Payments (Id, EmployeeId, PaymentDate, AccountNumber, FirstDateOccupied, LastDateOccupied, TotalDays,
+--AmountCharged, TaxRate, TaxAmount, PaymentTotal, Notes)
+--· Occupancies (Id, EmployeeId, DateOccupied, AccountNumber, RoomNumber, RateApplied, PhoneCharge, Notes)
+--Set the most appropriate data types for each column.
+--Set a primary key to each table.
+--Populate each table with only 3 records.
+--Make sure the columns that are present in 2 tables would be of the same data type.
+--Consider which fields are always required and which are optional.
+--Submit your CREATE TABLE and INSERT statements as Run queries & check DB.
 
+CREATE DATABASE Hotel
 
+CREATE TABLE Employees (
+	Id INT PRIMARY KEY IDENTITY,
+	FirstName NVARCHAR(30) NOT NULL,
+	LastName NVARCHAR(30) NOT NULL,
+	Title NVARCHAR(50),
+	Notes NVARCHAR(50)
+)
+
+CREATE TABLE Customers (
+	AccountNumber INT PRIMARY KEY IDENTITY,
+	FirstName NVARCHAR(30) NOT NULL,
+	LastName NVARCHAR(30) NOT NULL,
+	PhoneNumber NVARCHAR(30),
+	EmergencyName NVARCHAR(30),
+	EmergencyNumber NVARCHAR(30),
+	Notes NVARCHAR(50)
+)
+
+CREATE TABLE RoomStatus (
+	RoomStatus NVARCHAR(50) PRIMARY KEY NOT NULL,
+	Notes NVARCHAR(50)
+)
+
+CREATE TABLE RoomTypes (
+	RoomType NVARCHAR(50) PRIMARY KEY NOT NULL,
+	Notes NVARCHAR(50)
+)
+
+CREATE TABLE BedTypes (
+	BedType NVARCHAR(50) PRIMARY KEY NOT NULL,
+	Notes NVARCHAR(50)
+)
+
+CREATE TABLE Rooms (
+	RoomNumber INT PRIMARY KEY NOT NULL,
+	RoomType NVARCHAR(50) FOREIGN KEY REFERENCES RoomTypes(RoomType) NOT NULL,
+	BedType NVARCHAR(50) FOREIGN KEY REFERENCES BedTypes(BedType) NOT NULL,
+	Rate DECIMAL(6,2) NOT NULL,
+	RoomStatus BIT NOT NULL,
+	Notes NVARCHAR(50)
+)
+
+CREATE TABLE Payments (
+	Id INT PRIMARY KEY IDENTITY,
+	EmployeeId INT FOREIGN KEY REFERENCES Employees(Id) NOT NULL,
+	PaymentDate DATETIME NOT NULL,
+	AccountNumber INT FOREIGN KEY REFERENCES Customers(AccountNumber) NOT NULL,
+	FirstDateOccupied DATE NOT NULL,
+	LastDateOccupied DATE NOT NULL,
+	TotalDays AS DATEDIFF(DAY, FirstDateOccupied, LastDateOccupied),
+	AmountCharged DECIMAL(7, 2) NOT NULL,
+	TaxRate DECIMAL(6,2) NOT NULL,
+	TaxAmount AS AmountCharged * TaxRate,
+	PaymentTotal AS AmountCharged + AmountCharged * TaxRate,
+	Notes NVARCHAR(50)
+)
+
+CREATE TABLE Occupancies (
+	Id INT PRIMARY KEY IDENTITY,
+	EmployeeId INT FOREIGN KEY REFERENCES Employees(Id) NOT NULL,
+	DateOccupied DATE NOT NULL,
+	AccountNumber INT FOREIGN KEY REFERENCES Customers(AccountNumber) NOT NULL,
+	RoomNumber INT FOREIGN KEY REFERENCES Rooms(RoomNumber) NOT NULL,
+	RateApplied DECIMAL(7, 2) NOT NULL,
+	PhoneCharge DECIMAL(8, 2) NOT NULL,
+	Notes NVARCHAR(50)
+)
+
+INSERT INTO Employees(FirstName, LastNAme) VALUES
+('Ivo', 'T'),
+('Ivan', 'G'),
+('Ivelin', 'F')
+
+INSERT INTO Customers(FirstName, LastName, PhoneNumber) VALUES
+('A', 'E', '+359888666555'),
+('Z', 'Q', '+359866444222'),
+('W', 'R', '+35977555333')
+
+INSERT INTO RoomStatus(RoomStatus) VALUES
+('occupied'),
+('non occupied'),
+('almost occupied')
+
+INSERT INTO RoomTypes(RoomType) VALUES
+('single'),
+('double'),
+('apartment')
+
+INSERT INTO BedTypes(BedType) VALUES
+('single'),
+('double'),
+('couch')
+
+INSERT INTO Rooms(RoomNumber, RoomType, BedType, Rate, RoomStatus) VALUES
+(111, 'single', 'single', 20.0, 1),
+(112, 'double', 'double', 30.0, 0),
+(132, 'apartment', 'double', 10.0, 1)
+
+INSERT INTO Payments(EmployeeId, PaymentDate, AccountNumber, FirstDateOccupied, LastDateOccupied, AmountCharged, TaxRate) VALUES
+(3, '2011-11-25', 2, '2017-11-30', '2017-12-04', 250.0, 0.2),
+(2, '2014-06-03', 3, '2014-06-06', '2014-06-09', 340.0, 0.2),
+(1, '2016-02-25', 2, '2016-02-27', '2016-03-04', 500.0, 0.2)
+
+INSERT INTO Occupancies(EmployeeId, DateOccupied, AccountNumber, RoomNumber, RateApplied, PhoneCharge) VALUES
+(2, '2015-01-01', 2, 111, 70.0, 12.54),
+(2, '2017-01-02', 3, 112, 40.0, 11.22),
+(3, '2018-01-03', 1, 132, 110.0, 10.05)
 
