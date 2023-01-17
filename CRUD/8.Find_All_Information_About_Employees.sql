78777e89b1601e104bd639c9cfbea79e4be87ec5
Stan <dimitrov.s.dev@gmail.com>
2023-01-17 23:46:57 +0200

    CRUD

---
+++ CRUD/8.Find_All_Information_About_Employees.sql
@@ -0,0 +1,7 @@
+-- Task 8 => Find All Information About Employees
+-- SQL query that finds all information about the employees whose job title is "Sales Representative"
+
+SELECT
+	*
+	FROM Employees
+	WHERE JobTitle = 'Sales Representative'
