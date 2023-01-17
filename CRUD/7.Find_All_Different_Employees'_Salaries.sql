3c8c4f437f47deb9fcf84461bbee49ec5a4e0d93
Stan <dimitrov.s.dev@gmail.com>
2023-01-17 23:34:30 +0200

    CRUD

---
+++ CRUD/7.Find_All_Different_Employees'_Salaries.sql
@@ -0,0 +1,7 @@
+-- Task 7 => Find All Different Employees' Salaries
+-- Create a SQL query that finds all different salaries of the employees.
+--Display the salaries only in one column, named "Salary
+
+SELECT
+	DISTINCT Salary
+	FROM Employees
