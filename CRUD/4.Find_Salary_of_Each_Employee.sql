cf23db4c064c481609a0b5f39e94467f88f0b394
Stan <dimitrov.s.dev@gmail.com>
2023-01-17 23:02:45 +0200

    CRUD

--- CRUD/4.Find_Salary_of_Each_Employee.sql
+++ CRUD/4.Find_Salary_of_Each_Employee.sql
@@ -1,5 +1,5 @@
 -- Task 4 => Find Salary of Each Employee
 -- Create a SQL query that finds the first name, last name and salary for each employee
 
-SELECT Salary
+SELECT FirstName, LastName, Salary
 	FROM Employees
