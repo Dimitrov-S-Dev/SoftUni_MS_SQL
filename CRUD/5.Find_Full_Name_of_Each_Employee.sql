c518caf093564b2361485e01e7c0fc2592f7c24f
Stan <dimitrov.s.dev@gmail.com>
2023-01-17 23:10:55 +0200

    CRUD

---
+++ CRUD/5.Find_Full_Name_of_Each_Employee.sql
@@ -0,0 +1,7 @@
+-- Task 5 => Find Full Name of Each Employee
+--Create a SQL query that finds the first, middle and last name for each employee.
+
+SELECT [FirstName],
+	   [MiddleName],
+	   [LastName]
+	FROM Employees
