0a6eead041f349df37dc9778ec123fc081d232b0
Stan <dimitrov.s.dev@gmail.com>
2023-01-18 09:14:13 +0200

    CRUD

---
+++ CRUD/15.Sort_Employees_Table.sql
@@ -0,0 +1,14 @@
+-- Task 15 => Sort Employees Table
+-- Create a SQL query that sorts all the records
+--in the Employees table by the following criteria:
+--· By salary in decreasing order
+--· Then by the first name alphabetically
+--· Then by the last name descending
+--· Then by middle name alphabetically
+
+SELECT *
+	FROM Employees
+	ORDER BY Salary DESC,
+			FirstName,
+			LastName DESC,
+			MiddleName
---
+++ CRUD/17.Create_View_Employees_with_Job_Titles.sql
@@ -0,0 +1,9 @@
+-- Task 17 => Create View Employees with Job Titles
+-- Create a SQL query that creates a view "V_EmployeeNameJobTitle"
+--with a full employee name and a job title.
+--When the middle name is NULL replace it with an empty string ('').
+
+CREATE VIEW V_EmployeeNameJobTitle AS
+	SELECT CONCAT(FirstName,' ', MiddleName, ' ', LastName) AS [Full Name],
+			JobTitle
+		FROM Employees
