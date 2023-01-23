4520de860415f16d13db85338f2253b769a82923
Stan <dimitrov.s.dev@gmail.com>
2023-01-23 11:09:08 +0200

    Exercise

--- CRUD/Exercise.sql
+++ CRUD/Exercise.sql
@@ -1,49 +1,49 @@
--- Task 2
+-- Task 2 Find All the Information About Departments
 
 SELECT *
 FROM Departments
 
 
--- Task 3
+-- Task 3 Find all Department Names
 
 SELECT
 	[Name]
   FROM Departments
 
--- Task 4
+-- Task 4 Find Salary of Each Employee
 
 SELECT [FirstName],
 	   [LastName],
 	   [Salary]
 	FROM Employees
 
--- Task 5
+-- Task 5 Find Full Name of Each Employee
 
 SELECT [FirstName],
 	   [MiddleName],
 	   [LastName]
 	FROM Employees
 
--- Task 6
+-- Task 6 Find Email Address of Each Employee
 
 SELECT CONCAT([FirstName], '.', [LastName], '@', 'softuni.bg')
 		AS [Full Email Address]
 	FROM Employees
 
--- Task 7
+-- Task 7 Find All Different Employees’ Salaries
 
 SELECT
 	DISTINCT Salary
 	FROM Employees
 
--- Task 8
+-- Task 8 Find All Information About Employees
 
 SELECT
 	*
 	FROM Employees
 	WHERE JobTitle = 'Sales Representative'
 
--- Task 9
+-- Task 9 Find Names of All Employees by Salary in Range
 
 SELECT
 		FirstName,
@@ -52,7 +52,7 @@ SELECT
 	FROM Employees
 	WHERE Salary BETWEEN 20000 AND 30000
 
--- Task 10
+-- Task 10 Find Names of All Employees
 
 SELECT
 	CONCAT(FirstName, ' ', MiddleName, ' ', LastName)
@@ -60,14 +60,14 @@ SELECT
 	FROM Employees
 	WHERE Salary = 25000 OR Salary = 14000 OR Salary = 12500 OR Salary = 23600
 
--- Task 11
+-- Task 11 Find All Employees Without a Manager
 
 SELECT FirstName,
 	   LastName
 	FROM Employees
 	WHERE ManagerID IS NULL
 
--- Task 12
+-- Task 12 Find All Employees with a Salary More Than 50000
 
 SELECT FirstName,
 	   LastName,
@@ -76,7 +76,7 @@ SELECT FirstName,
 	WHERE Salary > 50000
 	ORDER BY Salary DESC
 
--- Task 13
+-- Task 13 Find 5 Best Paid Employees
 
 SELECT TOP(5)
 	   FirstName,
@@ -84,14 +84,14 @@ SELECT TOP(5)
 	FROM Employees
 	ORDER BY Salary DESC
 
--- Task 14
+-- Task 14 Find All Employees Except Marketing
 
 SELECT FirstName,
 	   LastName
 	FROM Employees
 	WHERE DepartmentID != 4
 
--- Task 15
+-- Task 15 Sort Employees Table
 
 SELECT *
 	FROM Employees
@@ -100,7 +100,7 @@ SELECT *
 			LastName DESC,
 			MiddleName
 
--- Task 16
+-- Task 16 Create View Employees with Salaries
 
 CREATE VIEW V_EmployeesSalaries AS
 	SELECT FirstName,
@@ -108,7 +108,7 @@ CREATE VIEW V_EmployeesSalaries AS
 		   Salary
 		FROM Employees
 
--- Task 17
+-- Task 17 Create View Employees with Job Titles
 
 CREATE VIEW V_EmployeeNameJobTitle AS
 	SELECT CONCAT(FirstName,' ', MiddleName, ' ', LastName) AS [Full Name],
@@ -120,14 +120,14 @@ CREATE VIEW V_EmployeeNameJobTitle AS
 			JobTitle
 		FROM Employees
 
--- Task 18
+-- Task 18 Distinct Job Titles
 
 SELECT
 	DISTINCT JobTitle
 	FROM Employees
 
 
--- Task 19
+-- Task 19 Find First 10 Started Projects
 
 SELECT
 	TOP(10) *
@@ -135,7 +135,7 @@ SELECT
 	ORDER BY StartDate,
 			 [Name]
 
--- Task 20
+-- Task 20 Last 7 Hired Employees
 
 SELECT TOP(7)
 		FirstName,
@@ -144,7 +144,7 @@ SELECT TOP(7)
 	FROM Employees
 	ORDER BY HireDate DESC
 
--- Task 21
+-- Task 21 Increase Salaries
 
 UPDATE Employees
 	SET Salary *= 1.12
@@ -153,13 +153,13 @@ UPDATE Employees
 SELECT Salary
 	FROM Employees
 
--- Task 22
+-- Task 22 All Mountain Peaks
 
 SELECT PeakName
 	FROM Peaks
 	ORDER BY PeakName ASC
 
--- Task 23
+-- Task 23 Biggest Countries by Population
 
 SELECT TOP(30)
 		CountryName,
@@ -169,7 +169,7 @@ SELECT TOP(30)
 	ORDER BY [Population] DESC,
 			CountryName
 
--- Task 24
+-- Task 24 Countries and Currency (Euro / Not Euro)
 
 SELECT CountryName, CountryCode, Currency =
 CASE CurrencyCode
@@ -180,7 +180,7 @@ FROM Countries
 ORDER BY CountryName
 
 
--- Task 25
+-- Task 25 All Diablo Characters
 USE Diablo
 
 SELECT Name
