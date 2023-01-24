0a7dd2abd5d15353681bdc350ee1efb19ad24313
Stan <dimitrov.s.dev@gmail.com>
2023-01-24 06:59:05 +0200

    Exercise

--- Subqueries_and_Joins/Exercise.sql
+++ Subqueries_and_Joins/Exercise.sql
@@ -7,7 +7,7 @@ SELECT
 	e.AddressID,
 	a.AddressText
 	FROM Employees AS e
-	LEFT JOIN Addresses AS a
+	JOIN Addresses AS a
 	ON e.AddressID = a.AddressID
 	ORDER BY e.AddressID
 
@@ -20,11 +20,36 @@ SELECT
 	t.Name AS Town,
 	a.AddressText
 	FROM Employees AS e
-	JOIN Addresses AS a
-	ON e.AddressID = a.AddressID
-	JOIN Towns AS t
-	ON t.TownID = a.TownID
+	JOIN Addresses AS a ON e.AddressID = a.AddressID
+	JOIN Towns AS t ON a.TownID = t.TownID
 	ORDER BY e.FirstName,
-	e.LastName
+		e.LastName
 
+
+-- Task 3 Sales Employee
+
+SELECT
+	e.EmployeeID,
+	e.FirstName,
+	e.LastName,
+	d.Name AS DepartmentName
+	FROM Employees AS e
+	JOIN Departments AS d
+	ON e.DepartmentID = d.DepartmentID
+	WHERE d.Name = 'Sales'
+	ORDER BY e.EmployeeID
+
+-- Task 4 Employee Departments
+
+SELECT
+	TOP 5
+	e.EmployeeID,
+	e.FirstName,
+	e.Salary,
+	d.Name AS DepartmentName
+	FROM Employees AS e
+	JOIN Departments AS d
+	ON e.DepartmentID = d.DepartmentID
+	WHERE e.Salary > 15000
+	ORDER BY e.DepartmentID
 
