9cd89f1e478dd5cb93ad4f34da019767f8ee5979
Stan <dimitrov.s.dev@gmail.com>
2023-01-24 07:41:42 +0200

    Exercise

--- Subqueries_and_Joins/Exercise.sql
+++ Subqueries_and_Joins/Exercise.sql
@@ -61,3 +61,17 @@ SELECT
 	LEFT JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
 	WHERE ep.ProjectID IS NOT NULL
 	ORDER BY e.EmployeeID
+
+-- Task 6 Employees hired after
+
+SELECT
+	e.FirstName,
+	e.LastName,
+	e.HireDate,
+	d.Name AS DeptName
+	FROM Employees AS e
+	JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+	WHERE e.HireDate > 1999/01/01 AND d.Name IN('Sales','Finance')
+	ORDER BY e.HireDate
+
+
