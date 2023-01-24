0c89f69cdf5a90944f112dd2a51ae8995d919fde
Stan <dimitrov.s.dev@gmail.com>
2023-01-24 10:39:34 +0200

    Exercise

--- Subqueries_and_Joins/Exercise.sql
+++ Subqueries_and_Joins/Exercise.sql
@@ -160,3 +160,15 @@ SELECT
 	WHERE CountryCode IN ('BG','RU','US')
 	GROUP BY CountryCode
 
+--Task 14 Countries with Rivers
+
+SELECT
+	TOP 5
+	c.CountryName,
+	r.RiverName
+	FROM Countries AS c
+	LEFT JOIN CountriesRivers AS cr ON c.CountryCode = cr.CountryCode
+	LEFT JOIN Rivers AS r ON cr.RiverId = r.Id
+	WHERE c.ContinentCode = 'AF'
+	ORDER BY c.CountryName
+
