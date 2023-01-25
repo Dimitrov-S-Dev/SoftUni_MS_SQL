4718d08f350523cb3d2c33b8faf224f243be9d2d
Stan <dimitrov.s.dev@gmail.com>
2023-01-25 05:23:00 +0200

    Exercise

--- Indices_and_Data_Aggregation/Exercise.sql
+++ Indices_and_Data_Aggregation/Exercise.sql
@@ -10,3 +10,10 @@ SELECT
 	MAX(MagicWandSize) AS LongestMagicWand
 	FROM WizzardDeposits
 
+--Task 3 Longest Magic Wand Per Deposit Groups
+
+SELECT
+	DepositGroup,
+	MAX(MagicWandSize) AS LongestMagicWand
+	FROM WizzardDeposits
+	GROUP BY DepositGroup
