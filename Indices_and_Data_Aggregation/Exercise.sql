0658a62de1f8a986d921e62f01cb3bc85b55617a
Stan <dimitrov.s.dev@gmail.com>
2023-01-25 21:13:24 +0200

    Exercise

--- Indices_and_Data_Aggregation/Exercise.sql
+++ Indices_and_Data_Aggregation/Exercise.sql
@@ -91,3 +91,16 @@ SELECT LEFT(FirstName,1) AS FirstLetter
 	WHERE DepositGroup LIKE 'Troll%'
 	GROUP BY LEFT(FirstName, 1)
 	ORDER BY FirstLetter
+
+--Task 11 Average Interest
+
+SELECT
+	DepositGroup,
+	IsDepositExpired,
+	AVG(DepositInterest)
+	FROM WizzardDeposits
+	WHERE DepositStartDate > '1985-01-01'
+	GROUP BY DepositGroup, IsDepositExpired
+	ORDER BY DepositGroup DESC,
+	IsDepositExpired
+
