84afbbcef97147b2ed3c49c2926de5ec379c4cc6
Stan <dimitrov.s.dev@gmail.com>
2023-01-25 06:01:18 +0200

    Exercise

--- Indices_and_Data_Aggregation/Exercise.sql
+++ Indices_and_Data_Aggregation/Exercise.sql
@@ -17,3 +17,13 @@ SELECT
 	MAX(MagicWandSize) AS LongestMagicWand
 	FROM WizzardDeposits
 	GROUP BY DepositGroup
+
+--Task 4 Smallest Deposit Group Per Magic Wand Size
+
+SELECT
+	TOP 2
+	DepositGroup
+	FROM WizzardDeposits
+	GROUP BY DepositGroup
+	ORDER BY AVG(MagicWandSize)
+	
