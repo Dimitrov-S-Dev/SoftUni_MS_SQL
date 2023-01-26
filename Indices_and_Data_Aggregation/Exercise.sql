3925c641074680e91b82e1b7921d517282f10230
Stan <dimitrov.s.dev@gmail.com>
2023-01-26 07:45:35 +0200

    Exercise

--- Indices_and_Data_Aggregation/Exercise.sql
+++ Indices_and_Data_Aggregation/Exercise.sql
@@ -143,3 +143,11 @@ SELECT
 	FROM NewT
 	GROUP BY DepartmentID
 
+--Task 16 Employee Maximum Salaries
+
+SELECT
+	DepartmentID,
+	MAX(Salary) AS MaxSalary
+	FROM Employees
+	GROUP BY DepartmentID
+	HAVING MAX(Salary) NOT BETWEEN 30000 AND 70000
