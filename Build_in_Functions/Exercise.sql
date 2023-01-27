581bfe3ebef350cf3cc4459bbaec0ea4ecf6d74d
Stan <dimitrov.s.dev@gmail.com>
2023-01-27 14:54:41 +0200

    Exercise

--- Build_in_Functions/Exercise.sql
+++ Build_in_Functions/Exercise.sql
@@ -153,3 +153,24 @@ Select
 	DATEADD(Month, 1, OrderDate) AS [Deliver Due]
 FROM Orders
 
+
+--Task 19 People Table
+CREATE TABLE People (
+	Id INT PRIMARY KEY IDENTITY,
+	Name NVARCHAR(50) NOT NULL,
+	Birthdate DATETIME NOT NULL
+)
+
+INSERT INTO People VALUES
+('Viktor', '2000-12-07'),
+('Steven', '1992-09-10'),
+('Stephen', '1910-09-19'),
+('John', '2010-01-06')
+
+SELECT Name,
+	DATEDIFF(YEAR, Birthdate, GETDATE()) AS [Age in Years],
+	DATEDIFF(MONTH, Birthdate, GETDATE()) AS [Age in Months],
+	DATEDIFF(DAY, Birthdate, GETDATE()) AS [Age in Days],
+	DATEDIFF(MINUTE, Birthdate, GETDATE()) AS [Age in Minutes]
+ FROM People
+
