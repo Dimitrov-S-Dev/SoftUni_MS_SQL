41ddb12c36983d47f590a00558d66ff23ed707c5
Stan <dimitrov.s.dev@gmail.com>
2023-01-27 16:31:16 +0200

    Exercise

--- Functions_and_Stored_Procedures/Exercise.sql
+++ Functions_and_Stored_Procedures/Exercise.sql
@@ -12,10 +12,10 @@ END
 
 GO
 
---EXEC dbo.usp_GetEmployeesSalaryAbove35000
+--Do not paste this in Judge. It's for testing purpose only.
+EXEC dbo.usp_GetEmployeesSalaryAbove35000
 
 -- Task 2 Employees with Salary Above Number
-
 GO
 CREATE OR ALTER PROCEDURE usp_GetEmployeesSalaryAboveNumber @minSalary DECIMAL(18, 4)
 AS
@@ -28,5 +28,23 @@ BEGIN
 END
 
 GO
+--Do not paste this in Judge. It's for testing purpose only.
 EXEC dbo.usp_GetEmployeesSalaryAboveNumber 48100
+
+--Task 3 Town Names Starting With
+
+GO
+CREATE OR ALTER PROCEDURE usp_GetTownsStartingWit @townNames VARCHAR(20)
+AS
+BEGIN
+	SELECT
+	Name AS Town
+	FROM Towns
+	WHERE Name LIKE @townNames + '%'
+
+END
+GO
+--Do not paste this in Judge. It's for testing purpose only.
+EXEC dbo.usp_GetTownsStartingWit b
+
 
