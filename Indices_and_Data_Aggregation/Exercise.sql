2be470602cf6ecf349559ca4f5a76817cd689d73
Stan <dimitrov.s.dev@gmail.com>
2023-01-27 15:00:13 +0200

    Exercise

--- Indices_and_Data_Aggregation/Exercise.sql
+++ Indices_and_Data_Aggregation/Exercise.sql
@@ -176,3 +176,9 @@ SELECT tt.DepartmentID, tt.Salary AS ThirdHighestSalary
 	WHERE OrderedSalaries = 3
 	GROUP BY tt.DepartmentID, tt.Salary
 
+--Task 19 **Salary Challenge
+SELECT TOP(10) FirstName, LastName, DepartmentID FROM Employees AS e1
+WHERE Salary >
+	(SELECT AVG(Salary) FROM Employees AS e2
+	 WHERE e1.DepartmentID = e2.DepartmentID)
+ORDER BY DepartmentID
