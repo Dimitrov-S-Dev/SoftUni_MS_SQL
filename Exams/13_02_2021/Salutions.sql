d411f76c571fe787c9724092baea3963ad214de9
Stan <dimitrov.s.dev@gmail.com>
2023-01-29 14:32:18 +0200

    Exams

--- Exams/13_02_2021/Salutions.sql
+++ Exams/13_02_2021/Salutions.sql
@@ -140,3 +140,16 @@ WHERE RepositoryId = (
 					FROM Repositories
 					WHERE Name = 'Softuni-Teamwork')
 
+--Task 5 Commits
+
+SELECT
+	Id,
+	[Message],
+	RepositoryId,
+	ContributorId
+	FROM Commits
+	ORDER BY Id,
+	[Message],
+	RepositoryId,
+	ContributorId
+
