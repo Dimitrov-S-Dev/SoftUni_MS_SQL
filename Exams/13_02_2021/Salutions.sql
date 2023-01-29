df4001038bb89f9439aef84c7e52321a785add3e
Stan <dimitrov.s.dev@gmail.com>
2023-01-29 18:43:11 +0200

    Exams

--- Exams/13_02_2021/Salutions.sql
+++ Exams/13_02_2021/Salutions.sql
@@ -165,3 +165,13 @@ SELECT
 	Id,
 	Name
 
+--Task 7 Issue Assignment
+
+SELECT
+	i.Id,
+	CONCAT_WS(':',u.Username, i.Title) AS IssueAssignee
+	FROM Issues AS i
+	JOIN Users AS u ON i.AssigneeId = u.Id
+	ORDER BY i.Id DESC,
+	i.AssigneeId
+
