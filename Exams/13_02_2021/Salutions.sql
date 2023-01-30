b6f30356313eb234812ed5f6d6f031464c8a01f9
Stan <dimitrov.s.dev@gmail.com>
2023-01-30 10:21:01 +0200

    Exams

--- Exams/13_02_2021/Salutions.sql
+++ Exams/13_02_2021/Salutions.sql
@@ -208,8 +208,9 @@ SELECT
 	u.Username,
 	AVG(f.Size) AS Size
 	FROM Users AS u
-	LEFT JOIN Commits AS c ON u.Id = c.ContributorId
-	LEFT JOIN Files AS f ON f.CommitId = c.Id
-	GROUP BY u.Username, c.ContributorId
+	JOIN Commits AS c ON u.Id = c.ContributorId
+	JOIN Files AS f ON f.CommitId = c.Id
+	GROUP BY u.Username
 	ORDER BY Size DESC,
 		u.Username
+
