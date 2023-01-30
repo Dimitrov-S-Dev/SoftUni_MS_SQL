f1484b663b8fb1d25dc7a87527a790d5687f3d4e
Stan <dimitrov.s.dev@gmail.com>
2023-01-30 23:33:14 +0200

    Exams

--- Subqueries_and_Joins/Exercise.sql
+++ Subqueries_and_Joins/Exercise.sql
@@ -229,11 +229,8 @@ SELECT TOP 5
            ISNULL(P.Elevation, 0) AS [Highest Peak Elevation],
            ISNULL(m.MountainRange, '(no mountain)') AS [Mountain]
       FROM Countries c
- LEFT JOIN MountainsCountries mc
-        ON mc.CountryCode = c.CountryCode
- LEFT JOIN Mountains m
-        ON m.Id = mc.MountainId
- LEFT JOIN Peaks p
-        ON p.MountainId = mc.MountainId
-  ORDER BY c.CountryName,
+      LEFT JOIN MountainsCountries mc ON mc.CountryCode = c.CountryCode
+      LEFT JOIN Mountains m ON m.Id = mc.MountainId
+      LEFT JOIN Peaks p ON p.MountainId = mc.MountainId
+      ORDER BY c.CountryName,
            P.PeakName
