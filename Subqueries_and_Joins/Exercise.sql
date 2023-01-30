56a5b42cab05c6335f3243eec598ff40a8adab56
Stan <dimitrov.s.dev@gmail.com>
2023-01-30 23:32:14 +0200

    Exams

--- Subqueries_and_Joins/Exercise.sql
+++ Subqueries_and_Joins/Exercise.sql
@@ -214,12 +214,12 @@ SELECT TOP(5)
 		 MAX(r.Length) AS LongestRiverLength
          FROM Countries AS c
         LEFT JOIN CountriesRivers AS cr ON c.CountryCode = cr.CountryCode
-LEFT JOIN Rivers AS r ON cr.RiverId = r.Id
-LEFT JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
-LEFT JOIN Mountains AS m ON mc.MountainId = m.Id
-LEFT JOIN Peaks AS p ON mc.MountainId = p.MountainId
-GROUP BY c.CountryName
-ORDER BY HighestPeakElevation DESC, LongestRiverLength DESC, CountryName
+        LEFT JOIN Rivers AS r ON cr.RiverId = r.Id
+        LEFT JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
+        LEFT JOIN Mountains AS m ON mc.MountainId = m.Id
+        LEFT JOIN Peaks AS p ON mc.MountainId = p.MountainId
+        GROUP BY c.CountryName
+        ORDER BY HighestPeakElevation DESC, LongestRiverLength DESC, CountryName
 
 --Task 18 Highest Peak Name and Elevation by Country
 
