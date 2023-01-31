0aedbba4db6542cfd52436021f914fcdf25f5962
Stan <dimitrov.s.dev@gmail.com>
2023-01-31 15:15:28 +0200

    Exercise

--- Triggers_and_Transactions/Exercise.sql
+++ Triggers_and_Transactions/Exercise.sql
@@ -27,6 +27,29 @@ END
 
 --Task 2 Create Table Emails
 
+CREATE TABLE NotificationEmails (
+    Id INT NOT NULL IDENTITY(1, 1),
+    Recipient INT NOT NULL,
+    Subject NVARCHAR(50) NOT NULL,
+    Body NVARCHAR(100) NOT NULL,
+
+    CONSTRAINT PK_NotificationEmails PRIMARY KEY (Id),
+    CONSTRAINT FK_NotificationEmails_Accounts FOREIGN KEY (Recipient) REFERENCES Accounts (Id)
+)
+GO
+
+CREATE TRIGGER tr_EmailNotification
+ON Logs
+AFTER INSERT
+AS
+BEGIN
+    INSERT INTO NotificationEmails (Recipient, Subject, Body)
+         SELECT i.AccountId,
+                CONCAT('Balance change for account: ', i.AccountId),
+                CONCAT('On ', GETDATE(), ' your balance was changed from ', i.OldSum, ' to ', i.NewSum, '.')
+           FROM INSERTED i
+END
+
 
 --Task 3 Deposit Money
 
