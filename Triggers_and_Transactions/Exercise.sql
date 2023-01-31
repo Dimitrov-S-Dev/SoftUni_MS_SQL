3083d6f223500399aedeced322a73e2081ef84ac
Stan <dimitrov.s.dev@gmail.com>
2023-01-31 15:20:44 +0200

    Exercise

--- Triggers_and_Transactions/Exercise.sql
+++ Triggers_and_Transactions/Exercise.sql
@@ -252,6 +252,26 @@ ORDER BY [Item Name]
 
 --Task 8 Employees with Three Projects
 
+CREATE PROC usp_AssignProject(@EmployeeId INT, @ProjectID INT)
+AS
+BEGIN
+    BEGIN TRANSACTION
+    DECLARE @EmployeeProjects INT
+    SET @EmployeeProjects = (SELECT COUNT(ep.ProjectID)
+                               FROM EmployeesProjects ep
+                              WHERE ep.EmployeeID = @EmployeeId)
+    IF (@EmployeeProjects >= 3)
+    BEGIN
+        RAISERROR('The employee has too many projects!', 16, 1)
+        ROLLBACK
+        RETURN
+    END
+
+    INSERT INTO EmployeesProjects (EmployeeID, ProjectID)
+         VALUES (@EmployeeId, @ProjectID)
+    COMMIT
+END
+
 
 --Task 9 Delete Employees
 
