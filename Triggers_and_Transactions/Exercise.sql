1f5d93c7ea8d7ebe74e0221b844e993ca851ce09
Stan <dimitrov.s.dev@gmail.com>
2023-01-31 15:18:11 +0200

    Exercise

--- Triggers_and_Transactions/Exercise.sql
+++ Triggers_and_Transactions/Exercise.sql
@@ -78,7 +78,24 @@ END
 
 --Task 5 Money Transfer
 
+CREATE PROC usp_TransferMoney (@SenderId INT, @ReceiverId INT, @Amount DECIMAL(18, 4))
+AS
+BEGIN
+    BEGIN TRANSACTION
+        IF (@Amount <= 0)
+        BEGIN
+            RAISERROR('Amount cannot be negative or zero', 16, 1)
+            ROLLBACK
+            RETURN
+        END
+        EXEC usp_DepositMoney @ReceiverId, @Amount
+        EXEC usp_WithdrawMoney @SenderId, @Amount
+        COMMIT
+END
+
+
 --Task 6 Trigger
+
 
 --Task 7 Massive Shopping
 
