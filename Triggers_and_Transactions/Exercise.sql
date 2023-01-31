62f579a1f0b815dc4b09a4a2b6266b6603b7c1c6
Stan <dimitrov.s.dev@gmail.com>
2023-01-31 15:14:27 +0200

    Exercise

--- Triggers_and_Transactions/Exercise.sql
+++ Triggers_and_Transactions/Exercise.sql
@@ -1,5 +1,29 @@
 --Task 1 Create Table logs
 
+CREATE TABLE Logs (
+    LogId INT NOT NULL IDENTITY(1, 1),
+    AccountId INT NOT NULL,
+    OldSum DECIMAL(18, 2),
+    NewSum DECIMAL(18, 2),
+
+    CONSTRAINT PK_Logs PRIMARY KEY (LogId),
+    CONSTRAINT FK_Logs_Accounts FOREIGN KEY (AccountId) REFERENCES Accounts(Id)
+)
+
+CREATE TRIGGER tr_ChangeBalance
+ON Accounts
+AFTER UPDATE
+AS
+BEGIN
+    INSERT INTO Logs (AccountId, OldSum, NewSum)
+         SELECT i.AccountHolderId,
+                d.Balance,
+                i.Balance
+           FROM INSERTED i
+     INNER JOIN DELETED d
+             ON d.AccountHolderId = i.AccountHolderId
+END
+
 
 --Task 2 Create Table Emails
 
