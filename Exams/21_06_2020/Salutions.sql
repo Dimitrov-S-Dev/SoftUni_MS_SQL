a694f7a2b5063b523be33e4ecabba39549482ecd
Stan <dimitrov.s.dev@gmail.com>
2023-02-01 17:24:03 +0200

    Exam

--- Exams/21_06_2020/Salutions.sql
+++ Exams/21_06_2020/Salutions.sql
@@ -1,1 +1,60 @@
 --Task 1 DDL Database Design
+
+CREATE DATABASE TripService
+
+USE TripService
+
+CREATE TABLE Cities
+(
+Id INT PRIMARY KEY IDENTITY,
+Name NVARCHAR(20) NOT NULL,
+CountryCode VARCHAR(2) NOT NULL
+)
+
+CREATE TABLE Hotels
+
+(
+Id INT PRIMARY KEY IDENTITY,
+Name NVARCHAR(20) NOT NULL,
+CityId INT REFERENCES Cities(Id) NOT NULL,
+EmployeeCount INT NOT NULL,
+BaseRate DECIMAL(4,2)
+)
+
+CREATE TABLE Rooms
+(
+Id INT PRIMARY KEY IDENTITY,
+Price DECIMAL (5,2) NOT NULL,
+Type NVARCHAR(20) NOT NULL,
+Beds INT NOT NULL,
+HotelId INT REFERENCES Hotels(Id)
+)
+
+CREATE TABLE Trips
+(
+Id INT PRIMARY KEY IDENTITY,
+RoomId INT REFERENCES Rooms (ID) NOT NULL,
+BookDate DATE NOT NULL,
+ArrivalDate DATE NOT NULL CHECK('ArrivalDate' <'ReturnDate' ),
+ReturnDate DATE NOT NULL CHECK('ReturnDate' > 'ArrivalDate'),
+CancelDate Date
+)
+
+CREATE TABLE Accounts
+(
+Id INT PRIMARY KEY IDENTITY,
+FirstName NVARCHAR(50) NOT NULL,
+MiddleName NVARCHAR(20),
+LastName NVARCHAR(50) NOT NULL,
+CityId INT REFERENCES Cities(Id) NOT NULL,
+BirthDate DATE NOT NULL,
+Email VARCHAR(100) NOT NULL UNIQUE
+)
+
+CREATE TABLE AccountsTrips
+(
+AccountId INT REFERENCES Accounts(Id),
+TripId INT REFERENCES Trips(Id),
+Luggage INT NOT NULL CHECK(Luggage > 0)
+PRIMARY KEY(AccountId, TripId)
+)
