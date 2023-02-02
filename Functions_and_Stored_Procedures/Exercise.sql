b15deefd32046184ccdf64f994c245995a02fa64
Stan <dimitrov.s.dev@gmail.com>
2023-02-03 01:14:50 +0200

    Exam

--- Functions_and_Stored_Procedures/Exercise.sql
+++ Functions_and_Stored_Procedures/Exercise.sql
@@ -63,24 +63,18 @@ GO
 
 -- Task 5 Salary Level Function
 
-CREATE FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18,4)
+CREATE FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18,4))
 RETURNS VARCHAR(8)
 AS
 BEGIN
 	DECLARE @salaryLevel VARCHAR(8)
+	SET @salaryLevel =
+	CASE
+		WHEN @salary < 30000 THEN 'Low'
+		WHEN @salary < 50000 THEN 'Average'
+		ELSE 'High'
+	END
 
-	IF @salary < 30000
-	BEGIN
-		SET @salaryLevel = 'Low'
-	END
-	ELSE IF @salary BETWEEN 30000 AND 50000
-	BEGIN
-		SET @salaryLevel = 'Average'
-	END
-	ELSE IF @salary > 50000
-	BEGIN
-	SET @salaryLevel = 'High'
-	END
 	RETURN @salaryLevel
 END
 
