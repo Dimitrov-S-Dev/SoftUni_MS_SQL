9d24b1fcbb1c513b067d009fd5f75abeeb03cc36
Stan <dimitrov.s.dev@gmail.com>
2023-02-02 16:26:13 +0200

    Exam

--- Exams/21_06_2020/Salutions.sql
+++ Exams/21_06_2020/Salutions.sql
@@ -147,3 +147,21 @@ SELECT
 	GROUP BY c.Id, c.Name, c.CountryCode
 	ORDER BY Accounts DESC
 
+--Task 9 Romantic Getaways
+
+SELECT
+	a.Id,
+	a.Email,
+	c.Name AS City,
+	COUNT(*) AS Trips
+	FROM AccountsTrips AS at
+	JOIN Accounts AS a ON at.AccountId = a.Id
+	JOIN Cities AS c ON a.CityId = c.Id
+	JOIN Trips AS t ON at.TripId = t.Id
+	JOIN Rooms as r ON t.RoomId = r.Id
+	JOIN Hotels AS h ON r.HotelId = h.Id
+	WHERE a.CityId = h.CityId
+	GROUP BY a.Id, a.Email, c.Name
+	ORDER BY Trips DESC,
+	a.Id
+
