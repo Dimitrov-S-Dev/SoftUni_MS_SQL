462590c58ddec1548b4cae5608a7013f36b43793
Stan <dimitrov.s.dev@gmail.com>
2023-02-02 11:05:11 +0200

    Exam

--- Exams/21_06_2020/Salutions.sql
+++ Exams/21_06_2020/Salutions.sql
@@ -95,4 +95,17 @@ DELETE
 	FROM AccountsTrips
 	WHERE AccountId = 47
 
+--Task 5 EEE-Email
+
+SELECT
+	a.FirstName,
+	a.LastName,
+	FORMAT(a.BirthDate, 'MM-dd-yyyy'),
+	c.Name AS Hometown,
+	a.Email
+	FROM Accounts AS a
+	JOIN Cities AS c ON a.CityId = c.Id
+	WHERE a.Email LIKE 'e%'
+	ORDER BY c.Name
+
 
