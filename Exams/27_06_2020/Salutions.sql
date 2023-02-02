21368303c77adbe8133ad27b908cd6b01b0173a4
Stan <dimitrov.s.dev@gmail.com>
2023-02-02 21:08:02 +0200

    Exam

--- Exams/27_06_2020/Salutions.sql
+++ Exams/27_06_2020/Salutions.sql
@@ -1,8 +1,94 @@
---Task 1 DDL Database Design
 ----Task 1 DDL Database Design
+
 CREATE DATABASE WMS
 GO
 
 USE WMS
 GO
+
+CREATE TABLE Clients
+(
+ClientId INT PRIMARY KEY IDENTITY,
+FirstName VARCHAR(50) NOT NULL,
+LastName VARCHAR(50) NOT NULL,
+Phone CHAR(12) CHECK(LEN(Phone)= 12) NOT NULL
+)
+
+GO
+
+CREATE TABLE Mechanics
+(
+MechanicId INT PRIMARY KEY IDENTITY,
+FirstName VARCHAR(50) NOT NULL,
+LastName VARCHAR(50) NOT NULL,
+Address VARCHAR(255) NOT NULL
+)
+
+GO
+
+
+CREATE TABLE Models
+(
+ModelId INT PRIMARY KEY IDENTITY,
+Name VARCHAR(50) UNIQUE NOT NULL
+)
+
+GO
+
+CREATE TABLE Jobs
+(
+JobId INT PRIMARY KEY IDENTITY,
+ModelId INT REFERENCES Models(ModelId) NOT NULL,
+Status VARCHAR(11) DEFAULT 'Pending' CHECK(Status IN('Pending', 'In Progress','Finished')) NOT NULL,
+ClienId INT REFERENCES Clients(ClientId) NOT NULL,
+MechaicId INT REFERENCES Mechanics(MechanicId),
+IssueDate DATE NOT NULL,
+FinishDate DATE
+)
+
+GO
+
+CREATE TABLE Orders
+(
+OrderId INT PRIMARY KEY IDENTITY,
+JobId INT REFERENCES Jobs(JobId) NOT NULL,
+IssueDate DATE,
+Delivered BIT DEFAULT 0
+)
+GO
+
+CREATE TABLE Vendors
+(
+VendorId INT PRIMARY KEY IDENTITY,
+Name VARCHAR(50) UNIQUE NOT NULL
+)
+GO
+
+CREATE TABLE Parts
+(
+PartId INT PRIMARY KEY IDENTITY,
+SerialNumber VARCHAR(50) UNIQUE NOT NULL,
+Description VARCHAR(255),
+Price DECIMAL(6, 2) CHECK(Price > 0 AND Price <= 9999.99) NOT NULL,
+VendorId INT REFERENCES Vendors(VendorId) NOT NULL,
+StockQty INT DEFAULT 0 CHECK(StockQty >= 0)
+)
+GO
+
+CREATE TABLE OrderParts
+(
+OrderId INT REFERENCES Orders(OrderId),
+PartId INT REFERENCES Parts(PartId),
+PRIMARY KEY(OrderId, PartId),
+Quantity INT DEFAULT 1 CHECK(Quantity >= 1)
+)
+GO
+
+CREATE TABLE PartsNeeded
+(
+JobId INT REFERENCES Jobs(JobId),
+PartId INT REFERENCES Parts(PartId),
+PRIMARY KEY(JobId, PartId),
+Quantity INT DEFAULT 1 CHECK(Quantity >= 1)
+)
 
