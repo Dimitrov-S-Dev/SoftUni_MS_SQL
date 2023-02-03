942813d48913b350616e055f932228375dce9258
Stan <dimitrov.s.dev@gmail.com>
2023-02-03 11:49:47 +0200

    Exam

--- Exams/27_06_2020/Salutions.sql
+++ Exams/27_06_2020/Salutions.sql
@@ -137,3 +137,16 @@ SELECT
 	ORDER BY m.MechanicId,
 	j.IssueDate,
 	j.JobId
+
+--Task 6 Current Clients
+
+SELECT
+	CONCAT_WS(' ', c.FirstName, c.LastName) AS Client,
+	DATEDIFF(DAY, j.IssueDate,'2017-04-24') AS [Days going],
+	j.Status
+	FROM Clients AS c
+	JOIN Jobs AS j ON c.ClientId = j.ClientId
+	WHERE Status != 'Finished'
+	ORDER BY [Days going] DESC,
+	Client
+
