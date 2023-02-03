e81a20f386c6805c2a0d62eafe5f0d38e5d7057a
Stan <dimitrov.s.dev@gmail.com>
2023-02-03 16:21:42 +0200

    Exam

--- Exams/27_06_2020/Salutions.sql
+++ Exams/27_06_2020/Salutions.sql
@@ -181,13 +181,13 @@ FROM Mechanics AS m
 --Task 9 Past Expenses
 
 SELECT
-	j.JobId,
-	ISNULL(SUM(p.Price *op.Quantity), 0) AS Total
-	FROM Parts AS p
-	JOIN OrderParts AS op ON p.PartId = op.PartId
-	JOIN Orders AS o ON op.OrderId = o.OrderId
-	JOIN Jobs AS j ON o.JobId = j.JobId
+	j.JobId AS JobId,
+	ISNULL(SUM(p.Price * op.Quantity),0) AS Total
+	FROM Jobs AS j
+	LEFT JOIN Orders AS o ON j.JobId = o.JobId
+	LEFT JOIN OrderParts AS op ON op.OrderId = o.OrderId
+	LEFT JOIN Parts AS p ON op.PartId = p.PartId
 	WHERE j.Status = 'Finished'
 	GROUP BY j.JobId
 	ORDER BY Total DESC,
-	j.JobId
+	JobId
