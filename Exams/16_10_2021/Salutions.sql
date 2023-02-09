1dbea98fa6aa456693517c9282b03bdf90f76c2e
Stan <dimitrov.s.dev@gmail.com>
2023-02-09 21:05:49 +0200

    Exams

--- Exams/16_10_2021/Salutions.sql
+++ Exams/16_10_2021/Salutions.sql
@@ -184,3 +184,25 @@ BEGIN
 	WHERE c.FirstName = 'Betty'
 	)
 END
+
+--Task 12
+
+GO
+CREATE PROCEDURE usp_SearchByTaste @taste VARCHAR(20)
+AS
+BEGIN
+		SELECT
+			c.CigarName,
+			CONCAT('$', c.PriceForSingleCigar) AS Price,
+			t.TasteType,
+			b.BrandName,
+			CONCAT(s.Length,' cm') AS CigarLength,
+			CONCAT(s.RingRange, ' cm') AS CigarRingRang
+			FROM Cigars AS c
+			JOIN Brands AS b ON c.BrandId = b.Id
+			JOIN Sizes AS s ON c.SizeId = s.Id
+			JOIN Tastes AS t ON c.TastId = t.Id
+			WHERE t.TasteType = @taste
+			ORDER BY CigarLength,
+			CigarRingRang DESC
+END
