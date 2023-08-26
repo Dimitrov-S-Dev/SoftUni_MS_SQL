e87a736eda73a559e9c010a143c65cc6bd063dca
Stan <dimitrov.s.dev@gmail.com>
2023-08-26 16:18:44 +0300

    MS_SQL

--- 3.Table Relations/Exercise.sql
+++ 3.Table Relations/Exercise.sql
@@ -6,25 +6,25 @@ USE One_To_One
 
 CREATE TABLE Passports
 (
-[PassportID] INT PRIMARY KEY IDENTITY(101,1),
-[PassportNumber] VARCHAR(10) NOT NULL
-
+    [PassportID] INT PRIMARY KEY IDENTITY(101,1),
+    [PassportNumber] VARCHAR(10) NOT NULL
 )
 
 CREATE TABLE [Persons]
 (
-[PersonID] INT PRIMARY KEY IDENTITY,
-[FirstName] NVARCHAR(30) NOT NULL,
-[Salary] DECIMAL(8,2) NOT NULL,
-[PassportID] INT REFERENCES Passports([PassportID]) UNIQUE NOT NULL
+    [PersonID] INT PRIMARY KEY IDENTITY,
+    [FirstName] NVARCHAR(30) NOT NULL,
+    [Salary] DECIMAL(8,2) NOT NULL,
+    [PassportID] INT REFERENCES Passports([PassportID]) UNIQUE NOT NULL
 )
 
 
-INSERT INTO [Passports] ([PassportNumber])
-	VALUES
-('N34FG21B'),
-('K65LO4R7'),
-('ZE657QP2')
+INSERT INTO
+    [Passports] ([PassportNumber])
+VALUES
+    ('N34FG21B'),
+    ('K65LO4R7'),
+    ('ZE657QP2')
 
 
 INSERT INTO [Persons]([FirstName], [Salary], [PassportID])
