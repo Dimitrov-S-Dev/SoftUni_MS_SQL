fa2d4c1ef52d38f3707d67274f5346add1367636
Stan <dimitrov.s.dev@gmail.com>
2023-08-29 23:41:00 +0300

    MS_SQL

--- 5.Subqueries and Joins/Exercise.sql
+++ 5.Subqueries and Joins/Exercise.sql
@@ -1,31 +1,33 @@
 --Task 1 Employee Address
 
 SELECT
-	TOP 5
-	e.EmployeeID,
+TOP 5
+    e.EmployeeID,
 	e.JobTitle,
 	e.AddressID,
 	a.AddressText
-	FROM Employees AS e
-	JOIN Addresses AS a
-	ON e.AddressID = a.AddressID
-	ORDER BY e.AddressID
+FROM
+    Employees AS e
+JOIN Addresses AS a ON e.AddressID = a.AddressID
+ORDER BY
+    e.AddressID
 
 ----------------------------------------------------
 
 --Task 2 Addresses with Towns
 
 SELECT
-	TOP 50
+TOP 50
 	e.FirstName,
 	e.LastName,
 	t.Name AS Town,
 	a.AddressText
-	FROM Employees AS e
-	JOIN Addresses AS a ON e.AddressID = a.AddressID
-	JOIN Towns AS t ON a.TownID = t.TownID
-	ORDER BY e.FirstName,
-		e.LastName
+FROM Employees AS e
+JOIN Addresses AS a ON e.AddressID = a.AddressID
+JOIN Towns AS t ON a.TownID = t.TownID
+ORDER BY
+    e.FirstName,
+	e.LastName
 
 ----------------------------------------------------
 
@@ -36,9 +38,8 @@ SELECT
 	e.FirstName,
 	e.LastName,
 	d.Name AS DepartmentName
-	FROM Employees AS e
-	JOIN Departments AS d
-	ON e.DepartmentID = d.DepartmentID
+FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
 	WHERE d.Name = 'Sales'
 	ORDER BY e.EmployeeID
 
