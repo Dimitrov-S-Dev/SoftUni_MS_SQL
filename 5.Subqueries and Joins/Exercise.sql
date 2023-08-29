49240f0c84bbce455cc2ee46be3fedaefdbd0cca
Stan <dimitrov.s.dev@gmail.com>
2023-08-30 01:21:33 +0300

    MS_SQL

--- 5.Subqueries and Joins/Exercise.sql
+++ 5.Subqueries and Joins/Exercise.sql
@@ -40,35 +40,43 @@ SELECT
 	d.Name AS DepartmentName
 FROM Employees AS e
 JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-	WHERE d.Name = 'Sales'
-	ORDER BY e.EmployeeID
+WHERE
+    d.Name = 'Sales'
+ORDER BY
+    e.EmployeeID
 
 ----------------------------------------------------
 
 --Task 4 Employee Departments
 
 SELECT
-	TOP 5
+TOP 5
 	e.EmployeeID,
 	e.FirstName,
 	e.Salary,
 	d.Name AS DepartmentName
-	FROM Employees AS e
-	JOIN Departments AS d
-	ON e.DepartmentID = d.DepartmentID
-	WHERE e.Salary > 15000
-	ORDER BY e.DepartmentID
+FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE
+    e.Salary > 15000
+ORDER BY
+    e.DepartmentID
 
 ----------------------------------------------------
 
 --Task 5 Employees without Project
 
 SELECT
-	TOP 3 e.EmployeeID, e.FirstName
-	FROM Employees AS e
-	LEFT JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
-	WHERE ep.ProjectID IS NOT NULL
-	ORDER BY e.EmployeeID
+TOP 3
+    e.EmployeeID,
+    e.FirstName,
+	ep.ProjectID
+FROM Employees AS e
+LEFT JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
+WHERE
+   ep.ProjectID IS NULL
+ORDER BY
+    e.EmployeeID
 
 ----------------------------------------------------
 --Task 6 Employees Hired After
@@ -78,10 +86,12 @@ SELECT
 	e.LastName,
 	e.HireDate,
 	d.Name AS DeptName
-	FROM Employees AS e
-	JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-	WHERE e.HireDate > '1999-01-01' AND d.Name IN('Sales','Finance')
-	ORDER BY e.HireDate
+FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE
+    e.HireDate > '1999-01-01' AND d.Name IN('Sales','Finance')
+ORDER BY
+    e.HireDate
 
 ----------------------------------------------------
 
