c17d17287e9fd54be98ccef2c48812b7257957b6
Stan <dimitrov.s.dev@gmail.com>
2023-08-30 14:43:58 +0300

    MS_SQL

--- 5.Subqueries and Joins/Exercise.sql
+++ 5.Subqueries and Joins/Exercise.sql
@@ -1,15 +1,14 @@
 --Task 1 Employee Address
 
-SELECT
-    TOP 5
-    e.EmployeeID,
-    e.JobTitle,
-    e.AddressID,
-    a.AddressText
-FROM Employees AS e
-LEFT JOIN Addresses AS a ON e.AddressID = a.AddressID
-ORDER BY
-    e.AddressID
+SELECT TOP 5
+       e.EmployeeID,
+       e.JobTitle,
+       e.AddressID,
+       a.AddressText
+  FROM Employees AS e
+  LEFT JOIN Addresses AS a
+    ON e.AddressID = a.AddressID
+ ORDER BY e.AddressID
 
 ----------------------------------------------------
 
@@ -17,32 +16,31 @@ ORDER BY
 
 SELECT
 TOP 50
-	e.FirstName,
-	e.LastName,
-	t.Name AS Town,
-	a.AddressText
-FROM Employees AS e
-JOIN Addresses AS a ON e.AddressID = a.AddressID
-JOIN Towns AS t ON a.TownID = t.TownID
-ORDER BY
-    e.FirstName,
-	e.LastName
+       e.FirstName,
+       e.LastName,
+       t.Name AS Town,
+       a.AddressText
+  FROM Employees AS e
+  JOIN Addresses AS a
+    ON e.AddressID = a.AddressID
+  JOIN Towns AS t
+    ON a.TownID    = t.TownID
+ ORDER BY e.FirstName,
+          e.LastName
 
 ----------------------------------------------------
 
 --Task 3 Sales Employee
 
-SELECT
-	e.EmployeeID,
-	e.FirstName,
-	e.LastName,
-	d.Name AS DepartmentName
-FROM Employees AS e
-JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-WHERE
-    d.Name = 'Sales'
-ORDER BY
-    e.EmployeeID
+SELECT e.EmployeeID,
+       e.FirstName,
+       e.LastName,
+       d.Name AS DepartmentName
+  FROM Employees AS e
+  JOIN Departments AS d
+    ON e.DepartmentID = d.DepartmentID
+ WHERE d.Name = 'Sales'
+ ORDER BY e.EmployeeID
 
 ----------------------------------------------------
 
@@ -50,16 +48,15 @@ ORDER BY
 
 SELECT
 TOP 5
-	e.EmployeeID,
-	e.FirstName,
-	e.Salary,
-	d.Name AS DepartmentName
-FROM Employees AS e
-JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-WHERE
-    e.Salary > 15000
-ORDER BY
-    e.DepartmentID
+       e.EmployeeID,
+       e.FirstName,
+       e.Salary,
+       d.Name AS DepartmentName
+  FROM Employees AS e
+  JOIN Departments AS d
+    ON e.DepartmentID = d.DepartmentID
+ WHERE e.Salary > 15000
+ ORDER BY e.DepartmentID
 
 ----------------------------------------------------
 
@@ -67,30 +64,28 @@ ORDER BY
 
 SELECT
 TOP 3
-    e.EmployeeID,
-    e.FirstName,
-	ep.ProjectID
-FROM Employees AS e
-LEFT JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
-WHERE
-   ep.ProjectID IS NULL
-ORDER BY
-    e.EmployeeID
+       e.EmployeeID,
+       e.FirstName,
+       ep.ProjectID
+  FROM Employees AS e
+  LEFT JOIN EmployeesProjects AS ep
+    ON e.EmployeeID = ep.EmployeeID
+ WHERE ep.ProjectID IS NULL
+ ORDER BY e.EmployeeID
 
 ----------------------------------------------------
 --Task 6 Employees Hired After
 
-SELECT
-	e.FirstName,
-	e.LastName,
-	e.HireDate,
-	d.Name AS DeptName
-FROM Employees AS e
-JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
-WHERE
-    e.HireDate > '01-01-1999' AND d.Name IN('Sales','Finance')
-ORDER BY
-    e.HireDate
+SELECT e.FirstName,
+       e.LastName,
+       e.HireDate,
+       d.Name AS DeptName
+  FROM Employees AS e
+  JOIN Departments AS d
+    ON e.DepartmentID = d.DepartmentID
+ WHERE e.HireDate > '01-01-1999'
+   AND d.Name IN ( 'Sales', 'Finance' )
+ ORDER BY e.HireDate
 
 ----------------------------------------------------
 
@@ -98,47 +93,46 @@ ORDER BY
 
 SELECT
 TOP 5
-	  e.EmployeeID,
-	  e.FirstName,
-	  p.Name
-FROM
-	EmployeesProjects AS ep
-JOIN Employees AS e ON e.EmployeeID = ep.EmployeeID
-JOIN Projects AS p ON ep.ProjectID = p.ProjectID
-WHERE
-	p.StartDate > '08-13-2002' AND p.EndDate IS NULL
-ORDER BY
-	e.EmployeeID
+       e.EmployeeID,
+       e.FirstName,
+       p.Name
+  FROM EmployeesProjects AS ep
+  JOIN Employees AS e
+    ON e.EmployeeID = ep.EmployeeID
+  JOIN Projects AS p
+    ON ep.ProjectID = p.ProjectID
+ WHERE p.StartDate > '08-13-2002'
+   AND p.EndDate IS NULL
+ ORDER BY e.EmployeeID
 
 ----------------------------------------------------
 
 --Task 8 Employee 24
 
-SELECT
-	e.EmployeeID,
-	e.FirstName,
-CASE
-	WHEN p.StartDate >= '01-01-2005' THEN NULL
-	ELSE p.Name
-	END AS ProjectName
-FROM Employees AS e
-JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
-JOIN Projects AS p ON ep.ProjectID = p.ProjectID
-WHERE
-    e.EmployeeID = 24
+SELECT e.EmployeeID,
+       e.FirstName,
+       CASE
+            WHEN p.StartDate >= '01-01-2005' THEN NULL
+            ELSE p.Name END AS ProjectName
+  FROM Employees AS e
+  JOIN EmployeesProjects AS ep
+    ON e.EmployeeID = ep.EmployeeID
+  JOIN Projects AS p
+    ON ep.ProjectID = p.ProjectID
+ WHERE e.EmployeeID = 24
 
 ----------------------------------------------------
 
 --Task 9 Employee Manager
 
-SELECT
-	e.EmployeeID,
-	e.FirstName,
-	e.ManagerID,
-	e2.FirstName AS ManagerName
-	FROM Employees AS e
-	JOIN Employees AS e2 ON e.ManagerID = e2.EmployeeID
-	WHERE e.ManagerID IN(3,7)
+SELECT e.EmployeeID,
+       e.FirstName,
+       e.ManagerID,
+       e2.FirstName AS ManagerName
+  FROM Employees AS e
+  JOIN Employees AS e2
+    ON e.ManagerID = e2.EmployeeID
+ WHERE e.ManagerID IN ( 3, 7 )
 
 ----------------------------------------------------
 
