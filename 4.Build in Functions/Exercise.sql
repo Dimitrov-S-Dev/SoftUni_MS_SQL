c568cca31c5649b4ba9a40a66f2eb8906e272e1a
Stan <dimitrov.s.dev@gmail.com>
2023-08-31 13:46:47 +0300

    MS_SQL

--- 4.Build in Functions/Exercise.sql
+++ 4.Build in Functions/Exercise.sql
@@ -1,64 +1,48 @@
 --Task 1 Find Names of All Employees by First Name
 
-SELECT
-    FirstName,
-    LastName
-FROM
-    Employees
-	--WHERE FirstName LIKE 'Sa%'
-WHERE
-    LEFT(FirstName, 2) = 'Sa'
+SELECT FirstName,
+       LastName
+  FROM Employees
+ --WHERE FirstName LIKE 'Sa%'
+ WHERE LEFT(FirstName, 2) = 'Sa'
 
 ----------------------------------------------------
 
 --Task 2 Find Names of All employees by Last Name
 
-SELECT
-    FirstName,
-    LastName
-FROM
-    Employees
-	--WHERE LastName LIKE '%ei%'
-WHERE
-    CHARINDEX('ei', LastName) != 0
+SELECT FirstName,
+       LastName
+  FROM Employees
+ --WHERE LastName LIKE '%ei%'
+ WHERE CHARINDEX('ei', LastName) != 0
 
 ----------------------------------------------------
 
 --Task 3 Find First Names of All Employees
 
-SELECT
-    FirstName
-FROM
-    Employees
-WHERE
-    DepartmentID IN (3, 10)
-AND
-    YEAR(HireDate) BETWEEN 1995 AND 2005
+SELECT FirstName
+  FROM Employees
+ WHERE DepartmentID IN ( 3, 10 )
+   AND YEAR(HireDate) BETWEEN 1995 AND 2005
 
 ----------------------------------------------------
 
 --Task 4 Find All Employees Except Engineers
 
-SELECT
-    FirstName,
-    LastName
-FROM
-    Employees
-WHERE
-    JobTitle NOT LIKE '%engineer%'
+SELECT FirstName,
+       LastName,
+       JobTitle
+  FROM Employees
+ WHERE JobTitle NOT LIKE '%engineer%'
 
 ----------------------------------------------------
 
 --Task 5 Find Towns with Name Length
 
-SELECT
-    [Name]
-FROM
-    Towns
-WHERE
-    LEN([Name]) IN (5, 6)
-ORDER BY
-    [Name]
+SELECT [Name]
+  FROM Towns
+ WHERE LEN([Name]) IN ( 5, 6 )
+ ORDER BY [Name]
 
 ----------------------------------------------------
 
