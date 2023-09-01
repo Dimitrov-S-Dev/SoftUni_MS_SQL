da340200d886ec483018b5a330b0bbffc9f95658
Stan <dimitrov.s.dev@gmail.com>
2023-09-01 21:46:07 +0300

    MS_SQL

--- 6.Indices and Data Aggregation/Exercise.sql
+++ 6.Indices and Data Aggregation/Exercise.sql
@@ -80,20 +80,19 @@ SELECT DepositGroup,
 
 --Task 9 Age Groups
 
-SELECT a.AgeGroup, COUNT(*) AS WizardCount FROM
-	(SELECT
-		CASE
-		WHEN Age <= 10 THEN '[0-10]'
-		WHEN Age <= 20 THEN '[11-20]'
-		WHEN Age <= 30 THEN '[21-30]'
-		WHEN Age <= 40 THEN '[31-40]'
-		WHEN Age <= 50 THEN '[41-50]'
-		WHEN Age <= 60 THEN '[51-60]'
-		ELSE '[61+]'
-		END AS AgeGroup
-		FROM WizzardDeposits) AS a
-	GROUP BY a.AgeGroup
-	ORDER BY a.AgeGroup
+SELECT a.AgeGroup,
+       COUNT(*) AS WizardCount
+  FROM (   SELECT CASE
+                       WHEN Age <= 10 THEN '[0-10]'
+                       WHEN Age <= 20 THEN '[11-20]'
+                       WHEN Age <= 30 THEN '[21-30]'
+                       WHEN Age <= 40 THEN '[31-40]'
+                       WHEN Age <= 50 THEN '[41-50]'
+                       WHEN Age <= 60 THEN '[51-60]'
+                       ELSE '[61+]' END AS AgeGroup
+             FROM WizzardDeposits) AS a
+ GROUP BY a.AgeGroup
+ ORDER BY a.AgeGroup
 
 ----------------------------------------------------
 
