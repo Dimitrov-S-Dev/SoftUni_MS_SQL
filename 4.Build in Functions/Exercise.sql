1133792f9fb42a05a99f4e0f4984a29b18103ab9
Stan <dimitrov.s.dev@gmail.com>
2023-09-01 12:06:31 +0300

    MS_SQL

--- 4.Build in Functions/Exercise.sql
+++ 4.Build in Functions/Exercise.sql
@@ -171,29 +171,20 @@ SELECT Username,
 
 --Task 17 Show All Games with Duration and Part of the Da
 
-SELECT
-    [Name]
-As
-    Game,
-    [Part of the Day] =
-CASE
-	WHEN DATEPART(HOUR, [Start]) < 12 THEN 'Morning'
-	WHEN DATEPART(HOUR, [Start]) < 18 THEN 'Afternoon'
-	ELSE 'Evening'
-	END,
-    Duration =
-CASE
-	WHEN Duration <= 3 THEN 'Extra Short'
-	WHEN Duration <= 6 THEN 'Short'
-	WHEN Duration > 6 THEN 'Long'
-	ELSE 'Extra Long'
-	END
-FROM
-    Games
-ORDER BY
-    Game,
-    Duration,
-    [Part of the Day]
+SELECT [Name] As Game,
+       [Part of the Day] = CASE
+                                WHEN DATEPART(HOUR, [Start]) < 12 THEN 'Morning'
+                                WHEN DATEPART(HOUR, [Start]) < 18 THEN 'Afternoon'
+                                ELSE 'Evening' END,
+       Duration = CASE
+                       WHEN Duration <= 3 THEN 'Extra Short'
+                       WHEN Duration <= 6 THEN 'Short'
+                       WHEN Duration > 6 THEN 'Long'
+                       ELSE 'Extra Long' END
+  FROM Games
+ ORDER BY Game,
+          Duration,
+          [Part of the Day]
 
 ----------------------------------------------------
 
