2ea5ac5e59845c02cc0b7a7ac9cfd16d381431b4
Stan <dimitrov.s.dev@gmail.com>
2023-09-01 21:04:23 +0300

    MS_SQL

--- 6.Indices and Data Aggregation/Exercise.sql
+++ 6.Indices and Data Aggregation/Exercise.sql
@@ -1,26 +1,23 @@
 --Task 1 Record's Count
 
-SELECT
-	COUNT(*) AS Count
-	FROM WizzardDeposits
+SELECT COUNT(*) AS Count
+  FROM WizzardDeposits
 
 ----------------------------------------------------
 
 --Task 2 Longest Magic Wand
 
-SELECT
-	MAX(MagicWandSize) AS LongestMagicWand
-	FROM WizzardDeposits
+SELECT MAX(MagicWandSize) AS LongestMagicWand
+  FROM WizzardDeposits
 
 ----------------------------------------------------
 
 --Task 3 Longest Magic Wand Per Deposit Groups
 
-SELECT
-	DepositGroup,
-	MAX(MagicWandSize) AS LongestMagicWand
-	FROM WizzardDeposits
-	GROUP BY DepositGroup
+SELECT DepositGroup,
+       MAX(MagicWandSize) AS LongestMagicWand
+  FROM WizzardDeposits
+ GROUP BY DepositGroup
 
 ----------------------------------------------------
 
