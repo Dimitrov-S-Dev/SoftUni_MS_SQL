8efcab095f8246bdca8540edfdf249f09440a4dc
Stan <dimitrov.s.dev@gmail.com>
2023-09-04 15:26:57 +0300

    MS_SQL

--- 5.Subqueries and Joins/Exercise.sql
+++ 5.Subqueries and Joins/Exercise.sql
@@ -257,27 +257,23 @@ SELECT TOP 5
 --Task 18 Highest Peak Name and Elevation by Country
 
 SELECT
-	TOP 5
-	CountryName AS Country,
-	ISNULL(PeakName, 'no highest peak') AS [Highest Peak Name],
-	ISNULL(Elevation, 0) AS [Highest Peak Elevation],
-	ISNULL(MountainRange, 'no mountain') AS Mountain
-FROM(
-	SELECT
-       c.CountryName,
-       p.PeakName,
-       p.Elevation,
-       m.MountainRange,
-	   DENSE_RANK() OVER(PARTITION BY c.CountryName ORDER BY p.Elevation DESC) AS RN
-  FROM Countries c
-  LEFT JOIN MountainsCountries mc
-    ON mc.CountryCode = c.CountryCode
-  LEFT JOIN Mountains m
-    ON m.Id           = mc.MountainId
-  LEFT JOIN Peaks p
-    ON p.MountainId   = m.Id
-	) AS SubQ
-WHERE
-	SubQ.RN = 1
+       TOP 5
+       CountryName AS Country,
+       ISNULL(PeakName, 'no highest peak') AS [Highest Peak Name],
+       ISNULL(Elevation, 0) AS [Highest Peak Elevation],
+       ISNULL(MountainRange, 'no mountain') AS Mountain
+  FROM (   SELECT c.CountryName,
+                  p.PeakName,
+                  p.Elevation,
+                  m.MountainRange,
+                  DENSE_RANK() OVER (PARTITION BY c.CountryName ORDER BY p.Elevation DESC) AS RN
+             FROM Countries c
+             LEFT JOIN MountainsCountries mc
+               ON mc.CountryCode = c.CountryCode
+             LEFT JOIN Mountains m
+               ON m.Id           = mc.MountainId
+             LEFT JOIN Peaks p
+               ON p.MountainId   = m.Id) AS SubQ
+ WHERE SubQ.RN = 1
  ORDER BY Country,
           [Highest Peak Name]
